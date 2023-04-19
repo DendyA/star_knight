acdb24369ad5ccb050bc4a2bbbb6c8b6a84b2e46
DendyA <githubmain_dja@gsi.bc.ca>
2023-04-18 21:33:41 -0700

    Adding in test shaders and some of the shader subsystem framework. Current test shaders successfully compile with the bgfx shader compiler shaderc.

---
+++ src/shaders/shader_manager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by DendyA on 16/04/23.
+//
+
+#include "shader_manager.h"
---
+++ src/shaders/shader_manager.h
@@ -0,0 +1,15 @@
+//
+// Created by DendyA on 16/04/23.
+//
+
+#ifndef STAR_KNIGHT_SHADER_MANAGER_H
+#define STAR_KNIGHT_SHADER_MANAGER_H
+
+
+class ShaderManager
+{
+
+};
+
+
+#endif //STAR_KNIGHT_SHADER_MANAGER_H
