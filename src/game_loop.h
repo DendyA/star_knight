72d65a0ad4b411e82abbcb3c85eb4e44819a6ff3
DendyA <githubmain_dja@gsi.bc.ca>
2023-05-01 19:17:31 -0700

    Fixes #13. Moved the logic in main.cpp's main() into the GameLoop class which will be the main runner class for the engine.

---
+++ src/game_loop.h
@@ -0,0 +1,21 @@
+// Created on: 01/05/23.
+// Author: DendyA
+
+#ifndef STAR_KNIGHT_GAME_LOOP_H
+#define STAR_KNIGHT_GAME_LOOP_H
+
+namespace star_knight
+{
+
+    class GameLoop
+    {
+        public:
+            GameLoop();
+            ~GameLoop();
+
+            int mainLoop();
+    };
+
+} // star_knight
+
+#endif //STAR_KNIGHT_GAME_LOOP_H
