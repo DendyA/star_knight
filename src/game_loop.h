703b4e377e2374814894b89a8ff5f8431dc0767e
DendyA <githubmain_dja@gsi.bc.ca>
2023-05-03 22:09:02 -0700

    Fixes #13. Added function and class documentation to the GameLoop class.

--- src/game_loop.h
+++ src/game_loop.h
@@ -7,12 +7,28 @@
 namespace star_knight
 {
 
+    /** GameLoop class\n
+     * This class is the runner-type class of the game engine. It is responsible for calling initialization/destruction logic for the
+     * SDL and bgfx encapsulating classes, setting up and submitting shaders to the GPU, and running the main game loop.
+     * @todo Perhaps move initialization of SDL and bgfx class objects into the constructor and follow a similar pattern to the sk_window class?
+     */
     class GameLoop
     {
         public:
+            /** Constructor\n
+             * This is the default constructor.
+             */
             GameLoop();
+
+            /** Destructor\n
+             * This is the default destructor.
+             */
             ~GameLoop();
 
+            /** mainLoop\n
+             * This is the main loop which is responsible for everything from initialization, destruction and running of the game engine.
+             * @return Ending status of the main game loop. 0 for success, non-zero error code for failure.
+             */
             int mainLoop();
     };
 
