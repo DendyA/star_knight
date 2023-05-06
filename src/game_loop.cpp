4b4345c4b918719d86fdbe1ae93bebf4dedf3df2
DendyA <githubmain_dja@gsi.bc.ca>
2023-05-06 16:17:49 -0700

    Fixes #14. Removed the extra namespace definition on the source files. Addresses comments in the PR.

--- src/game_loop.cpp
+++ src/game_loop.cpp
@@ -9,182 +9,178 @@
 
 #include "game_loop.h"
 
-namespace star_knight
+star_knight::GameLoop::GameLoop()
 {
-    star_knight::GameLoop::GameLoop()
-    {
-        m_errorCode = kNoErr;
-        m_errorMessage = "";
+    m_errorCode = kNoErr;
+    m_errorMessage = "";
 
-        initializeSDLGameObjects();
-        initializebgfxGameObjects();
+    initializeSDLGameObjects();
+    initializebgfxGameObjects();
+}
+
+star_knight::GameLoop::~GameLoop()
+{
+    // When destroying GameLoop, we only want to call destorybgfx if the bgfxInitializer initialized properly because otherwise a fatal error occurs.
+    // These two error codes are the only ones that could be thrown before bgfx would initialize. Meaning as long as
+    // it isn't reporting these two error codes, then bgfx is safe to destroy.
+    if(m_bgfxInitializer.getErrorCode() != star_knight::Initializer::SKRendererInitErrCodes::kSDLNoManagementWindowInfoErr ||
+        m_bgfxInitializer.getErrorCode() != star_knight::Initializer::SKRendererInitErrCodes::kbgfxInitErr)
+    {
+        m_bgfxInitializer.destroybgfx();
     }
+}
 
-    star_knight::GameLoop::~GameLoop()
-    {
-        // When destroying GameLoop, we only want to call destorybgfx if the bgfxInitializer initialized properly because otherwise a fatal error occurs.
-        // These two error codes are the only ones that could be thrown before bgfx would initialize. Meaning as long as
-        // it isn't reporting these two error codes, then bgfx is safe to destroy.
-        if(m_bgfxInitializer.getErrorCode() != star_knight::Initializer::SKRendererInitErrCodes::kSDLNoManagementWindowInfoErr ||
-            m_bgfxInitializer.getErrorCode() != star_knight::Initializer::SKRendererInitErrCodes::kbgfxInitErr)
-        {
-            m_bgfxInitializer.destroybgfx();
-        }
-    }
+star_knight::GameLoop::SKGameLoopErrCodes
+star_knight::GameLoop::getErrorCode()
+{
+    return m_errorCode;
+}
 
-    GameLoop::SKGameLoopErrCodes
-    GameLoop::getErrorCode()
-    {
-        return m_errorCode;
-    }
+std::string
+star_knight::GameLoop::getErrorMessage()
+{
+    return m_errorMessage;
+}
 
-    std::string
-    GameLoop::getErrorMessage()
-    {
-        return m_errorMessage;
-    }
-
-    void
-    star_knight::GameLoop::initializeSDLGameObjects()
-    {
+void
+star_knight::GameLoop::initializeSDLGameObjects()
+{
 //    Since the SDL_Quit function is called in SKWindow's destructor, simply terminating the program will handle the shutdown of the SDL subsytems.
-        m_skWindow = star_knight::SKWindow();
+    m_skWindow = star_knight::SKWindow();
 
 //    This errors-out and returns immediately since there is not a ton to be done if SDL can't initialize.
-        if(m_skWindow.getErrorCode() != star_knight::SKWindow::SKWindowErrCodes::kNoErr)
-        {
-            saveError(m_skWindow.getErrorMessage(), kSDLGameObjectsInitErr);
-            return;
-        }
+    if(m_skWindow.getErrorCode() != star_knight::SKWindow::SKWindowErrCodes::kNoErr)
+    {
+        saveError(m_skWindow.getErrorMessage(), kSDLGameObjectsInitErr);
+        return;
+    }
 
-        m_skWindow.initSDLWindow();
+    m_skWindow.initSDLWindow();
 
 //    This errors-out and returns immediately since having a window fail to initialize means no game to display.
-        if(m_skWindow.getErrorCode() != star_knight::SKWindow::SKWindowErrCodes::kNoErr)
-        {
-            saveError(m_skWindow.getErrorMessage(), kSDLGameObjectsInitErr);
-            return;
-        }
+    if(m_skWindow.getErrorCode() != star_knight::SKWindow::SKWindowErrCodes::kNoErr)
+    {
+        saveError(m_skWindow.getErrorMessage(), kSDLGameObjectsInitErr);
+        return;
+    }
+}
+
+void
+star_knight::GameLoop::initializebgfxGameObjects()
+{
+    // Not setting error here since the issue was due to an SDL init error.
+    if(m_errorCode != kNoErr)
+    {
+        return;
     }
 
-    void
-    star_knight::GameLoop::initializebgfxGameObjects()
-    {
-        // Not setting error here since the issue was due to an SDL init error.
-        if(m_errorCode != kNoErr)
-        {
-            return;
-        }
-
-        m_bgfxInitializer = star_knight::Initializer(m_skWindow.getpwindow());
+    m_bgfxInitializer = star_knight::Initializer(m_skWindow.getpwindow());
 
 //    This errors-out and returns immediately since having no bgfx corresponds to the inability to display graphics.
-        if(m_bgfxInitializer.getErrorCode() != star_knight::Initializer::SKRendererInitErrCodes::kNoErr)
-        {
-            saveError(m_bgfxInitializer.getErrorMessage(), kbgfxGameObjectsInitErr);
-            return;
-        }
-
-        m_bgfxInitializer.initbgfxView();
+    if(m_bgfxInitializer.getErrorCode() != star_knight::Initializer::SKRendererInitErrCodes::kNoErr)
+    {
+        saveError(m_bgfxInitializer.getErrorMessage(), kbgfxGameObjectsInitErr);
+        return;
     }
 
-    void
-    GameLoop::handleKeyDownEvent(SDL_Event keyDownEvent)
+    m_bgfxInitializer.initbgfxView();
+}
+
+void
+star_knight::GameLoop::handleKeyDownEvent(SDL_Event keyDownEvent)
+{
+    switch(keyDownEvent.key.keysym.sym)
     {
-        switch(keyDownEvent.key.keysym.sym)
-        {
-            case SDLK_LEFT:
-                m_transformManager.view_translateX(0.1);
-                break;
-            case SDLK_RIGHT:
-                m_transformManager.view_translateX(-0.1);
-                break;
-            case SDLK_UP:
-                m_transformManager.view_translateY(0.1);
-                break;
-            case SDLK_DOWN:
-                m_transformManager.view_translateY(-0.1);
-                break;
-            default:
-                break;
-        }
+        case SDLK_LEFT:
+            m_transformManager.view_translateX(0.1);
+            break;
+        case SDLK_RIGHT:
+            m_transformManager.view_translateX(-0.1);
+            break;
+        case SDLK_UP:
+            m_transformManager.view_translateY(0.1);
+            break;
+        case SDLK_DOWN:
+            m_transformManager.view_translateY(-0.1);
+            break;
+        default:
+            break;
     }
+}
 
-    GameLoop::SKGameLoopErrCodes
-    star_knight::GameLoop::mainLoop()
+star_knight::GameLoop::SKGameLoopErrCodes
+star_knight::GameLoop::mainLoop()
+{
+
+    bgfx::VertexBufferHandle vertexBufferHandle = star_knight::ShaderManager::initVertexBuffer();
+    bgfx::IndexBufferHandle indexBufferHandle = star_knight::ShaderManager::initIndexBuffer();
+
+    bgfx::ProgramHandle programHandle{};
+
+    bool generateProgramStatus = star_knight::ShaderManager::generateProgram("vs_simple.bin","fs_simple.bin", programHandle);
+
+    if(!generateProgramStatus)
     {
+        saveError("GameLoop: Error while trying to generate shader program\n", kShaderManagerProgramGenerateErr);
 
-        bgfx::VertexBufferHandle vertexBufferHandle = star_knight::ShaderManager::initVertexBuffer();
-        bgfx::IndexBufferHandle indexBufferHandle = star_knight::ShaderManager::initIndexBuffer();
-
-        bgfx::ProgramHandle programHandle{};
-
-        bool generateProgramStatus = star_knight::ShaderManager::generateProgram("vs_simple.bin","fs_simple.bin", programHandle);
-
-        if(!generateProgramStatus)
-        {
-            saveError("GameLoop: Error while trying to generate shader program\n", kShaderManagerProgramGenerateErr);
-
-            // TODO(DendyA): If possible, make these handles member variables in this class and then these can be destroyed from the destructor.
-            bgfx::destroy(vertexBufferHandle);
-            bgfx::destroy(indexBufferHandle);
-
-            return kShaderManagerProgramGenerateErr;
-        }
-
-        m_transformManager = star_knight::TransformationManager();
-
-        m_transformManager.setTransformMatrix();
-
-        SDL_Event currEvent;
-        bool quit = false;
-
-        while(!quit)
-        {
-            while(SDL_PollEvent(&currEvent))
-            {
-                switch(currEvent.type)
-                {
-                    case SDL_QUIT:
-                        quit = true;
-                        break;
-                    case SDL_KEYDOWN:
-                        handleKeyDownEvent(currEvent);
-                        break;
-                    default:
-                        break;
-                }
-
-                // Set vertex and index buffer.
-                bgfx::setVertexBuffer(0, vertexBufferHandle);
-                bgfx::setIndexBuffer(indexBufferHandle);
-
-                // Set render states.
-                bgfx::setState(BGFX_STATE_DEFAULT);
-
-                // Submit primitive for rendering to view 0.
-                // FIXME(DendyA): When this is put into the main game loop, this causes a delay in closing the game window.
-                //  Related to issue #17.
-                bgfx::submit(0, programHandle);
-
-                m_transformManager.updateViewTransform(0);
-
-                bgfx::frame();
-            }
-        }
-
+        // TODO(DendyA): If possible, make these handles member variables in this class and then these can be destroyed from the destructor.
         bgfx::destroy(vertexBufferHandle);
         bgfx::destroy(indexBufferHandle);
-        bgfx::destroy(programHandle);
 
-        return kNoErr;
+        return kShaderManagerProgramGenerateErr;
     }
 
-    void
-    star_knight::GameLoop::saveError(const std::string &errorMessage, SKGameLoopErrCodes errorCode)
+    m_transformManager = star_knight::TransformationManager();
+
+    m_transformManager.setTransformMatrix();
+
+    SDL_Event currEvent;
+    bool quit = false;
+
+    while(!quit)
     {
-        m_errorMessage = errorMessage;
-        m_errorCode = errorCode;
+        while(SDL_PollEvent(&currEvent))
+        {
+            switch(currEvent.type)
+            {
+                case SDL_QUIT:
+                    quit = true;
+                    break;
+                case SDL_KEYDOWN:
+                    handleKeyDownEvent(currEvent);
+                    break;
+                default:
+                    break;
+            }
+
+            // Set vertex and index buffer.
+            bgfx::setVertexBuffer(0, vertexBufferHandle);
+            bgfx::setIndexBuffer(indexBufferHandle);
+
+            // Set render states.
+            bgfx::setState(BGFX_STATE_DEFAULT);
+
+            // Submit primitive for rendering to view 0.
+            // FIXME(DendyA): When this is put into the main game loop, this causes a delay in closing the game window.
+            //  Related to issue #17.
+            bgfx::submit(0, programHandle);
+
+            m_transformManager.updateViewTransform(0);
+
+            bgfx::frame();
+        }
     }
 
-} // star_knight
+    bgfx::destroy(vertexBufferHandle);
+    bgfx::destroy(indexBufferHandle);
+    bgfx::destroy(programHandle);
+
+    return kNoErr;
+}
+
+void
+star_knight::GameLoop::saveError(const std::string &errorMessage, SKGameLoopErrCodes errorCode)
+{
+    m_errorMessage = errorMessage;
+    m_errorCode = errorCode;
+}
--- src/renderer/transformation_manager.cpp
+++ src/renderer/transformation_manager.cpp
@@ -5,78 +5,75 @@
 
 #include "transformation_manager.h"
 
-namespace star_knight
-{
 //  m_coordinateSysUp, m_lookingAt, m_eyePosition HAVE to be initialized in an initializer list
 //  because the default constructor of Vec3 is set to delete (aka Vec3() = delete).
-    star_knight::TransformationManager::TransformationManager() :
-        m_coordinateSysUp(0.0f, 1.0f, 0.0f),
-        m_lookingAt(0.0f, 0.0f, 0.0f),
-        m_eyePosition(0.0f, 0.0f, 10.0f)
-    {
+star_knight::TransformationManager::TransformationManager() :
+    m_coordinateSysUp(0.0f, 1.0f, 0.0f),
+    m_lookingAt(0.0f, 0.0f, 0.0f),
+    m_eyePosition(0.0f, 0.0f, 10.0f)
+{
 //      Setting projection matrix parameters. This won't change very frequently, so the projection matrix will be able to be pre-computed.
-        m_fov = STARTING_FOV;
-        m_aspectRatio = STARTING_ASPECT_RATIO;
-        m_nearPlane = STARTING_NEAR_PLANE;
-        m_farPlane = STARTING_FAR_PLANE;
-    }
+    m_fov = STARTING_FOV;
+    m_aspectRatio = STARTING_ASPECT_RATIO;
+    m_nearPlane = STARTING_NEAR_PLANE;
+    m_farPlane = STARTING_FAR_PLANE;
+}
 
-    star_knight::TransformationManager::~TransformationManager() = default;
+star_knight::TransformationManager::~TransformationManager() = default;
 
-    void
-    star_knight::TransformationManager::setProjMatrix()
-    {
-        bx::mtxProj(m_projMat,
-                    m_fov,
-                    m_aspectRatio,
-                    m_nearPlane, m_farPlane,
-                    bgfx::getCaps()->homogeneousDepth);
-    }
+void
+star_knight::TransformationManager::setProjMatrix()
+{
+    bx::mtxProj(m_projMat,
+                m_fov,
+                m_aspectRatio,
+                m_nearPlane, m_farPlane,
+                bgfx::getCaps()->homogeneousDepth);
+}
 
-    void
-    star_knight::TransformationManager::setViewMatrix()
-    {
-        bx::mtxLookAt(m_viewMat, m_eyePosition, m_lookingAt, m_coordinateSysUp);
-    }
+void
+star_knight::TransformationManager::setViewMatrix()
+{
+    bx::mtxLookAt(m_viewMat, m_eyePosition, m_lookingAt, m_coordinateSysUp);
+}
 
-    // ViewId is just an uint16_t "under-the-hood".
-    void
-    star_knight::TransformationManager::updateViewTransform(bgfx::ViewId viewID)
-    {
-        setProjMatrix();
-        setViewMatrix();
+// ViewId is just an uint16_t "under-the-hood".
+void
+star_knight::TransformationManager::updateViewTransform(bgfx::ViewId viewID)
+{
+    setProjMatrix();
+    setViewMatrix();
 
-        bgfx::setViewTransform(viewID, m_viewMat, m_projMat);
-    }
+    bgfx::setViewTransform(viewID, m_viewMat, m_projMat);
+}
 
-    void
-    TransformationManager::view_translateX(float delta)
-    {
-        m_lookingAt.x += delta;
-        m_eyePosition.x += delta;
+void
+star_knight::TransformationManager::view_translateX(float delta)
+{
+    m_lookingAt.x += delta;
+    m_eyePosition.x += delta;
 
-    }
+}
 
-    void
-    TransformationManager::view_translateY(float delta)
-    {
-        m_lookingAt.y += delta;
-        m_eyePosition.y += delta;
-    }
+void
+star_knight::TransformationManager::view_translateY(float delta)
+{
+    m_lookingAt.y += delta;
+    m_eyePosition.y += delta;
+}
 
-    void
-    star_knight::TransformationManager::setTransformMatrix()
-    {
+void
+star_knight::TransformationManager::setTransformMatrix()
+{
 
-        float mtx[16];
-        bx::mtxRotateY(mtx, 0.0f);
+    float mtx[16];
+    bx::mtxRotateY(mtx, 0.0f);
 
-        // position x,y,z
-        mtx[12] = 0.0f;
-        mtx[13] = 0.0f;
-        mtx[14] = 0.0f;
+    // position x,y,z
+    mtx[12] = 0.0f;
+    mtx[13] = 0.0f;
+    mtx[14] = 0.0f;
 
-        // Set model matrix for rendering.
-        bgfx::setTransform(mtx);
-    }
-} // star_knight
+    // Set model matrix for rendering.
+    bgfx::setTransform(mtx);
+}
